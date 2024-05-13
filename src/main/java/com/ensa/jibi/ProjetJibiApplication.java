ce3239d077ecaef7347df02ee885ef3a06d3495d
Mehdi <mehdibaallal@gmail.com>
2024-05-13 15:05:36 +0100

    Blank Project

---
+++ src/main/java/com/ensa/jibi/ProjetJibiApplication.java
@@ -0,0 +1,13 @@
+package com.ensa.jibi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetJibiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetJibiApplication.class, args);
+	}
+
+}
